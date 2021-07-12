7817eea9415fb39ab30f0b8a76c0a3e38841cc36
0yusufokur0@gmail.com <0yusufokur0@gmail.com>
2021-07-12 15:51:48 +0300

    18 video bitti

--- app/src/main/java/com/resurrection/localveritabanuygulamas/KayitEkleGuncelle.java
+++ app/src/main/java/com/resurrection/localveritabanuygulamas/KayitEkleGuncelle.java
@@ -1,5 +1,10 @@
 package com.resurrection.localveritabanuygulamas;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +13,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -21,28 +27,36 @@ import android.widget.Toast;
 
 import com.blogspot.atifsoftwares.circularimageview.CircularImageView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageActivity;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class KayitEkleGuncelle extends AppCompatActivity {
     private CircularImageView profileResmi;
-    private EditText editAd,editTelefon,editMail,editDogumtarihi,editAciklama;
+    private EditText editAd, editTelefon, editMail, editDogumtarihi, editAciklama;
     private FloatingActionButton btnKayit;
     ActionBar actionBar;
     // izin sabitleri
-    private static final int  KAMERA_TALEP_KODU =100;
-    private static final int  Depolama_TALEP_KODU =101;
+    private static final int KAMERA_TALEP_KODU = 100;
+    private static final int Depolama_TALEP_KODU = 101;
     // resi seçin sabitleri
-    private static final int  KAMERADAN_RESİM_SECME_KODU = 102;
-    private static final int  GALERİDEN_RESİM_SECME_KODU = 103;
+    private static final int KAMERADAN_RESİM_SECME_KODU = 102;
+    private static final int GALERİDEN_RESİM_SECME_KODU = 103;
     //iziler dizi
     private String[] kameraIzınleri; // kamera ve depolama izin
     private String[] depolamaIzınleri; // depolama izin
     // resim tutucu
     private Uri resimUri;
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit_ekle_guncelle);
-        actionBar  = getSupportActionBar();
+
+
+        actionBar = getSupportActionBar();
         actionBar.setTitle("Kayıt ekle");
         actionBar.setDisplayShowHomeEnabled(true); // geri oku
         actionBar.setDisplayHomeAsUpEnabled(true);// geri oku
@@ -55,9 +69,12 @@ public class KayitEkleGuncelle extends AppCompatActivity {
         btnKayit = findViewById(R.id.btn_kayit);
 
         // izin dizileri tanımlama
-        kameraIzınleri = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        kameraIzınleri = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         depolamaIzınleri = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
+
+        //--------------------------------------------------------
+        //-----------------------------------------------------
 
 
         //btn kayita tıklama
@@ -77,34 +94,35 @@ public class KayitEkleGuncelle extends AppCompatActivity {
             }
         });
 
+
     }
 
     private void resimSecmeDialog() {
-        String[] ogeler = {"Kamera","Galeri"};
+        String[] ogeler = {"Kamera", "Galeri"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Resim sec");
         builder.setItems(ogeler, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if (which == 0){
+                if (which == 0) {
                     //Kamera tıklandığında
-                    if (!kameraIznıKontrolu()){
+                    if (!kameraIznıKontrolu()) {
                         //kamera
                         // kameraya erişim izni yoksa izni talep et
                         kameraIzınTalebi();
-                    }else {
+                    } else {
                         //kameradam resim sec
                         kameradanSec();
                     }
                 }
-                if (which == 1){
+                if (which == 1) {
                     //Galeri
                     //galeriye tıklandığında
-                    if (!depolamaIznıKontrolu()){
+                    if (!depolamaIznıKontrolu()) {
                         //kamera
                         // kameraya erişim izni yoksa izni talep et
                         depolamaIzınTalebi();
-                    }else {
+                    } else {
                         //galeriden resim sec
                         galeridenSec();
 
@@ -119,13 +137,14 @@ public class KayitEkleGuncelle extends AppCompatActivity {
     private void kameradanSec() {
         // kameradan secilen resim onActivty result metotu ile alınacak
         ContentValues degerler = new ContentValues();
-        degerler.put(MediaStore.Images.Media.TITLE,"Resim Başlığı");
-        degerler.put(MediaStore.Images.Media.DESCRIPTION,"Resim Açıklaması");
-        resimUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,degerler);
+        degerler.put(MediaStore.Images.Media.TITLE, "Resim Başlığı");
+        degerler.put(MediaStore.Images.Media.DESCRIPTION, "Resim Açıklaması");
+        resimUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, degerler);
 
         Intent kameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        kameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,resimUri);
-        startActivityForResult(kameraIntent,KAMERADAN_RESİM_SECME_KODU);
+        kameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, resimUri);
+        startActivityForResult(kameraIntent, KAMERADAN_RESİM_SECME_KODU);
+
     }
 
     // galeriden Açtırma
@@ -133,7 +152,7 @@ public class KayitEkleGuncelle extends AppCompatActivity {
         // galeriden secilen resim onActivty result metotu ile alınacak
         Intent galeriIntent = new Intent(Intent.ACTION_PICK);
         galeriIntent.setType("image/*"); // burada resim ve (/*)resim türlerindekiler anlamına geliyor
-        startActivityForResult(galeriIntent,GALERİDEN_RESİM_SECME_KODU);
+        startActivityForResult(galeriIntent, GALERİDEN_RESİM_SECME_KODU);
     }
 
     //ActionBardaki geriye tıklandığında geriye gitmesi
@@ -142,27 +161,73 @@ public class KayitEkleGuncelle extends AppCompatActivity {
         onBackPressed();// geri tuşuna basıldığında ne yapıyorsa onu yap
         return super.onSupportNavigateUp();
     }
+
     // depolam izni kontrolü
-    private boolean depolamaIznıKontrolu(){
-        boolean sonuc = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) ==( PackageManager.PERMISSION_GRANTED);
+    private boolean depolamaIznıKontrolu() {
+        boolean sonuc = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
         return sonuc;
     }
+
     // depolamaIznıTalebi
-    private void depolamaIzınTalebi(){
-        ActivityCompat.requestPermissions(this,depolamaIzınleri,Depolama_TALEP_KODU);
+    private void depolamaIzınTalebi() {
+        ActivityCompat.requestPermissions(this, depolamaIzınleri, Depolama_TALEP_KODU);
     }
+
     // depolam izni kontrolü
-    private boolean kameraIznıKontrolu(){
-        boolean sonuc1 = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) ==( PackageManager.PERMISSION_GRANTED);
-        boolean sonuc2 = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) ==( PackageManager.PERMISSION_GRANTED);
+    private boolean kameraIznıKontrolu() {
+        boolean sonuc1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        boolean sonuc2 = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
         return sonuc1 && sonuc2;
     }
+
     // kameraIznıTalebi
-    private void kameraIzınTalebi(){
-        ActivityCompat.requestPermissions(this,kameraIzınleri,KAMERA_TALEP_KODU);
+    private void kameraIzınTalebi() {
+        ActivityCompat.requestPermissions(this, kameraIzınleri, KAMERA_TALEP_KODU);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //KAMERADAN YADA ALINACAK RESİM BURADA İŞLENİP alınacak
+        System.out.println("buradayız");
+        if (resultCode == RESULT_OK) {
+            // resim seçildi
+            System.out.println("şuradayız");
+            if (requestCode == GALERİDEN_RESİM_SECME_KODU) {
+                System.out.println("oradayız");
+                // galeriden secilen resim
+                CropImage.activity(data.getData())
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1, 1)
+                        .start(this);
+            } else if (requestCode == KAMERADAN_RESİM_SECME_KODU) {
+                // kameradan secilen resim
+                CropImage.activity(resimUri)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1, 1)
+                        .start(this);
+            }
+            // resmi kırpıp profil resmine aktarma
+            else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                // resim kırpıldı
+                // kırpılan resmi al
+                // kırpılma başarılı ise
+                CropImage.ActivityResult sonuc = CropImage.getActivityResult(data);
+                if (resultCode == RESULT_OK) {
+                    resimUri = sonuc.getUri();
+                    //Resmş circle image viewe aktar
+                    profileResmi.setImageURI(resimUri);
+                }// kırpılma başarısızsa
+                else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+                    Exception hata = sonuc.getError();
+                    Toast.makeText(this, "hata hata", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
-
 
 
 
